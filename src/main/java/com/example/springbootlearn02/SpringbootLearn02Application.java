1c2d00ff3d9bc6ca2a74814cacead78cea892b54
youchen <1450207968@qq.com>
2020-07-27 16:16:41 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootlearn02/SpringbootLearn02Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootlearn02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearn02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearn02Application.class, args);
+    }
+
+}
