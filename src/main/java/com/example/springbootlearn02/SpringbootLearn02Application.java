18ab091e363218ebfb965463acafc583e9f02b66
youchen <1450207968@qq.com>
2020-07-28 11:42:51 +0800

    缓存快速体验结束

--- src/main/java/com/example/springbootlearn02/SpringbootLearn02Application.java
+++ src/main/java/com/example/springbootlearn02/SpringbootLearn02Application.java
@@ -5,6 +5,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * 一、搭建基本环境
+ *  1、创建JavaBeans封装数据
+ *  2、整合MyBatis操作数据库
+ *  3、整合MyBatis操作数据库
+ *      1、配置数据源信息
+ *      2、使用注解版的MyBatis
+ *          1）、@MapperScan指定需要扫描的mapper接口所在的包
+ *  二、快速体验缓存
+ *      步骤：
+ *          1、开启基于注解的缓存 @EnableCaching
+ *          2、标注缓存注解即可
+ *              @Cacheable
+ *              @CacheEvict
+ *              @CachePut
+ *   默认使用的是ConcurrentMapCacheManager==ConcurrentMapCache；将数据保存在	ConcurrentMap<Object, Object>中
+ */
 @EnableCaching
 @MapperScan("com.example.springbootlearn02.mapper")
 @SpringBootApplication
