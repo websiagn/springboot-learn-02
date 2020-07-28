afff449ed5cc7b0f25c0d064d666f14d56016abe
youchen <1450207968@qq.com>
2020-07-28 14:46:07 +0800

    配置Redis对象序列化

---
+++ src/main/java/com/example/springbootlearn02/config/MyRedisConfig.java
@@ -0,0 +1,26 @@
+package com.example.springbootlearn02.config;
+
+import com.example.springbootlearn02.bean.Employee;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.net.UnknownHostException;
+
+@Configuration
+public class MyRedisConfig {
+
+    // 配置Redis对象序列化
+    @Bean
+    public RedisTemplate<Object, Employee> empRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory
+    ) throws UnknownHostException {
+        RedisTemplate<Object, Employee> template = new RedisTemplate<Object, Employee>();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<Employee> serializer = new Jackson2JsonRedisSerializer<Employee>(Employee.class);
+        template.setDefaultSerializer(serializer);
+        return template;
+    }
+}
