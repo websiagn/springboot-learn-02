aaa1fb23ed7d71366df76beb2f0f48527de5b053
youchen <1450207968@qq.com>
2020-07-28 14:38:17 +0800

    配置Redis，基本使用

--- src/main/java/com/example/springbootlearn02/bean/Employee.java
+++ src/main/java/com/example/springbootlearn02/bean/Employee.java
@@ -1,6 +1,8 @@
 package com.example.springbootlearn02.bean;
 
-public class Employee {
+import java.io.Serializable;
+
+public class Employee implements Serializable {
     private Integer id;
     private String lastName;
     private Integer gender;
